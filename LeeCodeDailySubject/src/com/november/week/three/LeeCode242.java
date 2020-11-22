419b4fbf5aa91152db237f964552bc9e9ca17344
zmji <1473765655@qq.com>
2020-11-22 14:10:01 +0800

    2020/11/22 每日一题242 简单提交

---
+++ LeeCodeDailySubject/src/com/november/week/three/LeeCode242.java
@@ -0,0 +1,40 @@
+package com.november.week.three;
+
+/**
+ * 给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+ *
+ * 示例 1:
+ *
+ * 输入: s = "anagram", t = "nagaram"
+ * 输出: true
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/valid-anagram
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LeeCode242 {
+
+    /**
+     * 哈希表,使用数组存储，
+     * 若不是小写字母就使用map来存储，最后再遍历查看是否一致
+     * @param s
+     * @param t
+     * @return
+     */
+    public boolean isAnagram(String s, String t) {
+        int[] cnt1 = new int[26];
+        int[] cnt2 = new int[26];
+        for (char c : s.toCharArray()) {
+            cnt1[c - 'a']++;
+        }
+        for (char c : t.toCharArray()) {
+            cnt2[c - 'a']++;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (cnt1[i] != cnt2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
